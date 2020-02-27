03335557cfcaeaf013b4bebecd8ae465623005d9
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-02-27 17:43:56 -0300

    added project springboot

---
+++ src/main/java/com/coffeedev/apiGeekCentral/ApiGeekCentralApplication.java
@@ -0,0 +1,13 @@
+package com.coffeedev.apiGeekCentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeekCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGeekCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeedev/apiGeekCentral/ApiGeekCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeedev.apiGeekCentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGeekCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
