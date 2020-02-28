990d8b3906d465c0811fd7d2af4e882378330855
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-02-28 18:27:52 -0300

    created exception to objectNotFound

---
+++ src/main/java/com/coffeedev/apiGeekCentral/repositories/Exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.coffeedev.apiGeekCentral.repositories.Exception;
+
+public class ObjectNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause){
+        super(message,cause);
+    }
+}
