5ad3b4390731ca79593c1bee5d6856ddf946782f
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-02-28 18:24:03 -0300

    added findby of title

--- src/main/java/com/coffeedev/apiGeekCentral/repositories/MangaRepository.java
+++ src/main/java/com/coffeedev/apiGeekCentral/repositories/MangaRepository.java
@@ -3,5 +3,9 @@ package com.coffeedev.apiGeekCentral.repositories;
 import com.coffeedev.apiGeekCentral.domain.Manga;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MangaRepository extends JpaRepository<Manga,Integer> {
+
+    List<Manga> findByTitle(String title);
 }
