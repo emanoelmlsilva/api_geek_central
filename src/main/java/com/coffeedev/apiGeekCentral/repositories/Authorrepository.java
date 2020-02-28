8adedd3607990b7f5fde6ad278bd8a8e10f6354b
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-02-28 20:22:57 -0300

    renamed

--- src/main/java/com/coffeedev/apiGeekCentral/repositories/Authorrepository.java
+++
@@ -1,7 +0,0 @@
-package com.coffeedev.apiGeekCentral.repositories;
-
-import com.coffeedev.apiGeekCentral.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Authorrepository extends JpaRepository<Author,Integer> {
-}
