21717d3da2f674e33fd8ac07c616dfd849714452
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-02-28 20:29:42 -0300

    created achive configuration

---
+++ src/main/java/com/coffeedev/apiGeekCentral/Config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.coffeedev.apiGeekCentral.Config;
+
+import com.coffeedev.apiGeekCentral.services.BDService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private BDService dbService;
+
+    @Value("$$(spring.jpa.hibernate.ddl-auto)")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException{
+        if("create".equals(strategy)){
+            return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+
+}
