4c37168bbb8c99a26300ef012a023c5c5f7706fc
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-02-29 11:57:04 -0300

    added interface CommandLineRunner

--- src/main/java/com/coffeedev/apiGeekCentral/ApiGeekCentralApplication.java
+++ src/main/java/com/coffeedev/apiGeekCentral/ApiGeekCentralApplication.java
@@ -1,13 +1,17 @@
 package com.coffeedev.apiGeekCentral;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGeekCentralApplication {
+public class ApiGeekCentralApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGeekCentralApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
